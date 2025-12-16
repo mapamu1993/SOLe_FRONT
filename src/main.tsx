67359eff3329f5a7830ed2863169348780893589
Christian <christianldcvallejo@gmail.com>
2025-12-16 23:27:31 +0100

    feat: Add CartListDesign component for displaying cart items, managing quantities, and showing order confirmation.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client"; // 1. Importar la función real, no el tipo
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./features/auth/context/auth.context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SnackbarProvider } from "notistack"; // 1. IMPORTAR ESTO
 
 const rootElement = document.getElementById("root");
 
@@ -12,7 +13,6 @@ if (!rootElement) {
   throw new Error("No se encontró el elemento root");
 }
 
-// 2. Usar createRoot directamente desde react-dom/client
 const root = createRoot(rootElement);
 const queryClient = new QueryClient();
 
@@ -21,7 +21,12 @@ root.render(
     <AuthProvider>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <SnackbarProvider
+            maxSnack={3}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          >
+            <App />
+          </SnackbarProvider>
         </QueryClientProvider>
       </BrowserRouter>
     </AuthProvider>
