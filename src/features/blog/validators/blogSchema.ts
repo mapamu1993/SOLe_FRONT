c12cceb27606778d46156403c296d45957afbe01
Maria Pages <mariapages93@outlook.com>
2025-12-17 13:20:50 +0100

    cambios de main a mi rama, sin traerme el fallo de blogSchema

--- src/features/blog/validators/blogSchema.ts
+++ src/features/blog/validators/blogSchema.ts
@@ -3,11 +3,7 @@ import { z } from "zod";
 export const createBlogSchema = z.object({
   title: z.string().min(5, "El tìtulo debe contener al menos 5 caracteres."),
   content: z.string().min(30, "El contenido debe tener al menos 30 caracteres"),
-<<<<<<< HEAD
   image: z.any().optional(),
-=======
-  imageage: z.any().optional(),
->>>>>>> main
 });
 
 export type CreateBlogFields = z.infer<typeof createBlogSchema>;
