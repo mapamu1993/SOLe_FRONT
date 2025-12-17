3b597cb6cf443e8568222bfb8f46c5d010b2ffab
PAMELA <pamelaramosgarcia93@outlook.es>
2025-12-17 12:32:45 +0100

    blogImage to image - productImage to image

--- src/features/blog/validators/blogSchema.ts
+++ src/features/blog/validators/blogSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createBlogSchema = z.object({
   title: z.string().min(5, "El tìtulo debe contener al menos 5 caracteres."),
   content: z.string().min(30, "El contenido debe tener al menos 30 caracteres"),
-  blogImage: z.any().optional(),
+  imageage: z.any().optional(),
 });
 
 export type CreateBlogFields = z.infer<typeof createBlogSchema>;
